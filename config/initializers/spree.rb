15fb5ec21059dedb5154e620f3090c6f0599c3b0
Abe <shalashtein@gmail.com>
2020-11-16 17:25:51 +0200

    logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -69,7 +69,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus.svg"
+  config.logo = "logo.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -78,7 +78,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus.svg"
+  config.admin_interface_logo = "logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
