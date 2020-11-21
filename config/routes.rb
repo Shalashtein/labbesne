63bb58dcfef14ce9e6a81e2e95fdd753a6ab9500
Abe <shalashtein@gmail.com>
2020-11-21 17:26:41 +0200

    local seed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
-
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
